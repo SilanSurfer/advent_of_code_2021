7e548b1b5838ccd8408da61d07218d196bfc6aba
SilanSurfer <szwarc.adam@gmail.com>
2022-01-23 20:34:55 +0100

    [style] Corrected code formatting

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,15 +1,22 @@
-use std::io::{BufRead, BufReader};
-use std::fs::File;
 use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 type Result<T> = std::result::Result<T, anyhow::Error>;
 
 fn count_increase(data: impl IntoIterator<Item = u64>) -> u64 {
-    data.into_iter().tuple_windows().filter(|(prev, next)| prev < next).count() as _
+    data.into_iter()
+        .tuple_windows()
+        .filter(|(prev, next)| prev < next)
+        .count() as _
 }
 
 fn count_increase_window(data: impl IntoIterator<Item = u64>) -> u64 {
-    let trans_data: Vec<u64> = data.into_iter().tuple_windows::<(_, _, _)>().map(|(a, b, c)| a + b + c).collect();
+    let trans_data: Vec<u64> = data
+        .into_iter()
+        .tuple_windows::<(_, _, _)>()
+        .map(|(a, b, c)| a + b + c)
+        .collect();
     count_increase(trans_data)
 }
 
