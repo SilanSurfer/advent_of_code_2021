360a0e5673d894ca6a8797170ccd81a308d751c0
SilanSurfer <szwarc.adam@gmail.com>
2022-02-15 21:25:16 +0100

    [refactor] Changed solution 2 to simple one
    Corrected clippy warnings and code formatting.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -15,7 +15,7 @@ impl FromStr for Command {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let parts: Vec<&str> = s.split(" ").collect();
+        let parts: Vec<&str> = s.split(' ').collect();
         if parts.len() != 2 {
             return Err(anyhow::anyhow!("{} should have only 2 parts!", s));
         }
@@ -23,7 +23,7 @@ impl FromStr for Command {
             "forward" => Command::Forward(parts[1].parse()?),
             "up" => Command::Up(parts[1].parse()?),
             "down" => Command::Down(parts[1].parse()?),
-            _ => Err(anyhow::anyhow!("Unknown command: {}", parts[0]))?,
+            _ => return Err(anyhow::anyhow!("Unknown command: {}", parts[0])),
         };
         Ok(command)
     }
@@ -33,13 +33,29 @@ impl Command {
     pub fn apply(&self, mut position: (i64, i64)) -> (i64, i64) {
         match self {
             Command::Forward(x) => {
-                position.0 = position.0 + *x as i64;
+                position.0 += *x as i64;
             }
             Command::Up(x) => {
-                position.1 = position.1 - *x as i64;
+                position.1 -= *x as i64;
             }
             Command::Down(x) => {
-                position.1 = position.1 + *x as i64;
+                position.1 += *x as i64;
+            }
+        };
+        position
+    }
+
+    pub fn apply_with_aim(&self, mut position: (i64, i64, i64)) -> (i64, i64, i64) {
+        match self {
+            Command::Forward(x) => {
+                position.0 += *x as i64;
+                position.1 += position.2 * *x as i64;
+            }
+            Command::Up(x) => {
+                position.2 -= *x as i64;
+            }
+            Command::Down(x) => {
+                position.2 += *x as i64;
             }
         };
         position
@@ -54,12 +70,22 @@ fn read_input(reader: impl BufRead) -> Result<Vec<Command>> {
     Ok(data)
 }
 
-fn solution_1(input: Vec<Command>) -> i64 {
+fn solution_1(input: &[Command]) -> i64 {
     let mut position: (i64, i64) = (0, 0);
-    input.iter().for_each(|command| position = command.apply(position));
+    input
+        .iter()
+        .for_each(|command| position = command.apply(position));
 
     position.0 * position.1
+}
 
+fn solution_2(input: &[Command]) -> i64 {
+    let mut position: (i64, i64, i64) = (0, 0, 0);
+    input
+        .iter()
+        .for_each(|command| position = command.apply_with_aim(position));
+
+    position.0 * position.1
 }
 
 fn main() -> Result<()> {
@@ -67,8 +93,11 @@ fn main() -> Result<()> {
     let reader = BufReader::new(file);
     let data = read_input(reader)?;
 
-    let result_1 = solution_1(data);
+    let result_1 = solution_1(&data);
     println!("Solution for part 1: {}", result_1);
     assert_eq!(result_1, 2036120);
+
+    let result_2 = solution_2(&data);
+    println!("Solution for part 2: {}", result_2);
     Ok(())
 }
